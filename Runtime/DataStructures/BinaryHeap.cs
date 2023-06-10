f44dea3d3d28b5bd4881612b68c61f7787ed501d
Malcolm Neal <malcolm.neal@sphereentertainmentco.com>
2023-06-10 11:48:29 -0700

    added binary heap classes

---
+++ Runtime/DataStructures/BinaryHeap.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PixelGameLibrary.DataStructures
+{
+    public class Node<T>
+    {
+        public Node<T> parent;
+        public List<Node<T>> children;
+    }
+    
+    public class BinaryHeap<T>
+    {
+        private Node<T> root;
+        
+    }
+}
